74f83333080faff3e6031a6636c96b954872e5c2
D22127229 Aaron Christopher Dees <d22127229@ADAPT-MASTER.campus.tudublin.ie>
2024-12-13 15:09:12 +0000

    Update audiosplitting code

--- audio_splitting.py
+++ audio_splitting.py
@@ -1,20 +1,18 @@
 from pydub import AudioSegment
 from pydub.utils import make_chunks
-
-myaudio = AudioSegment.from_file("/Users/adees/Code/neural_granular_synthesis/datasets/ESC-50_SeaWaves/audio/samples/5secs/small_train/1-39901-A-11.wav" , "wav") 
-chunk_length_ms = 1000 # pydub calculates in millisec
-chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one sec
-
 import os
 
-directory = "/Users/adees/Code/neural_granular_synthesis/datasets/ESC-50_SeaWaves/audio/samples/5secs/small_train/"
+directory = "/home/CAMPUS/d22127229/code/github/neuralGranularSynthesis/data/EpidemicSound/water_wave_eval_train/"
     
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-    if filename.endswith(".wav"): 
+    if filename.endswith(".wav"):
+        myaudio = AudioSegment.from_file(directory+filename , "wav") 
+        chunk_length_ms = 1000 # pydub calculates in millisec
+        chunks = make_chunks(myaudio, chunk_length_ms) #Make chunks of one sec
         # print(filename)
         for i, chunk in enumerate(chunks):
-            chunk_name = f"/Users/adees/Code/neural_granular_synthesis/datasets/ESC-50_SeaWaves/audio/samples/5secs/small_split/{str(filename)[:-4]}_chunk{i}.wav"
+            chunk_name = f"/home/CAMPUS/d22127229/code/github/neuralGranularSynthesis/data/EpidemicSound/water_wave_chunked_eval_train/{str(filename)[:-4]}_chunk{i}.wav"
             chunk.export(chunk_name, format="wav")
         print("Chunked: ", os.path.join(directory, str(filename)))
         continue
